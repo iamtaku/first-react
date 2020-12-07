b8bb4be4663c00e92140d59dda1047659541edb9
Takuhei Mizutani <takuheim1011@gmail.com>
2020-12-07 14:15:20 +0900

    react init + first component

---
+++ src/MyInfo.js
@@ -0,0 +1,20 @@
+// import React from "react";
+
+function MyInfo() {
+  return (
+    <div>
+      <h1 className="name">Takuhei Mizutani</h1>
+      <p>
+        I recently graduated a coding bootcamp but I have no idea what I'm
+        supposed to do
+      </p>
+      <ul>
+        <li>Argentina</li>
+        <li>Thailand</li>
+        <li>Berlin</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MyInfo;
